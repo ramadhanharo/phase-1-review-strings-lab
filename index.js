1ee8e97a5072b0e93ce867b7c164b50220e650f4
ramadhanharo <ramadhan.haro@moringaschool.com>
2024-11-26 14:50:45 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Ramadhan Haro';
+const welcomeMessage = 'Welcome to Flatbook, Ramadhan Haro!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = '${R,Welcome}!'
